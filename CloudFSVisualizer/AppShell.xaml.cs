3b848a03cdc67d68b71f0380f9a17ea3e43926c1
lyzl <lyzl5764@hotmail.com>
2018-01-08 22:34:23 +0800

    Initial commit

---
+++ CloudFSVisualizer/AppShell.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using CloudFSVisualizer.Model;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace CloudFSVisualizer.Assets
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class AppShell : Page
+    {
+        List<NavigationPage> pages;
+        public AppShell()
+        {
+            this.InitializeComponent();
+            pages = new List<NavigationPage>();
+        }
+
+        private void AppShellNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.IsSettingsSelected)
+            {
+                AppContentFrame.Navigate(typeof(SettingPage));
+            }
+            else
+            {
+                var selectedItem = args.SelectedItem as NavigationPage;
+                AppContentFrame.Navigate(selectedItem.Dest);
+            }
+        }
+    }
+}
