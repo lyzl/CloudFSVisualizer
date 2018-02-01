06e2b800a4881590974a346846f17dbe7575eaca
lyzl <lyzl5764@hotmail.com>
2018-02-01 18:33:23 +0000

    fix the listDirectory deserialize function

--- CloudFSVisualizer/HDFSFileManager.cs
+++ CloudFSVisualizer/HDFSFileManager.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using CloudFSVisualizer.Model;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace CloudFSVisualizer
 {
@@ -13,11 +14,29 @@ namespace CloudFSVisualizer
     {
         public static async Task<FileStatus> GetFileStatus( HDFSFile file)
         {
-            var address = $@"http://{file.ServerHost}/webhdfs/v1/{file.Path}?op=GETFILESTATUS";
+            var address = $@"http://{file.ServerHost}:50070/webhdfs/v1/{file.Path}?op=GETFILESTATUS";
             var json = await NetworkManager.FetchStringDataFromUri(new Uri(address));
-            var dyn = JsonConvert.DeserializeObject<dynamic>(json);
-            var status = dyn["FileStatus"];
+            JObject rootObject = JObject.Parse(json);
+            JToken statusToken = rootObject["FileStatus"];
+            var status = statusToken.ToObject<FileStatus>();
             return status;
+        }
+
+        public static async Task<List<HDFSFile>> ListDirectory(HDFSFile file)
+        {
+            var address = $@"http://{file.ServerHost}:50070/webhdfs/v1/{file.Path}?op=LISTSTATUS";
+            var json = await NetworkManager.FetchStringDataFromUri(new Uri(address));
+            JObject rootObject = JObject.Parse(json);
+            IList<JToken> statusTokens = rootObject["FileStatuses"]["FileStatus"].Children().ToList();
+            List<FileStatus> statusList = new List<FileStatus>();
+            List<HDFSFile> fileList = new List<HDFSFile>();
+            foreach (var token in statusTokens)
+            {
+                var result = token.ToObject<FileStatus>();
+                fileList.Add(new HDFSFile(file, result.pathSuffix));
+            }
+
+            return fileList;
         }
     }
 }
