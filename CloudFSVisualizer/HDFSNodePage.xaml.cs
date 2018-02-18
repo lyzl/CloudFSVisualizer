57252079640f3f6c8e0d6e9e29daafc60e71e811
lyzl <lyzl5764@hotmail.com>
2018-02-18 23:48:08 +0000

    added periodic functions to update node performance

--- CloudFSVisualizer/HDFSNodePage.xaml.cs
+++ CloudFSVisualizer/HDFSNodePage.xaml.cs
@@ -13,6 +13,14 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Collections.ObjectModel;
+using Windows.UI.Core;
+using System.Threading.Tasks;
+using Telerik.Core;
+using Telerik.UI.Xaml.Controls.Chart;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -21,17 +29,73 @@ namespace CloudFSVisualizer
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class HDFSNodePage : Page
+    public sealed partial class HDFSNodePage : Page, INotifyPropertyChanged
     {
         public HDFSNode CurrentNode { get; set; }
+        public ObservableCollection<NodeOperatingSystem> SysInfoList { get; set; }
+        public Timer QueryTimer { get; set; }
+        public TimeSpan startTimeSpan = TimeSpan.Zero;
+        public TimeSpan periodicTimeSpan = TimeSpan.FromMilliseconds(1000);
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private AutoResetEvent _queryLock = new AutoResetEvent(false);
+
+        
+
         public HDFSNodePage()
         {
             this.InitializeComponent();
+            SysInfoList = new ObservableCollection<NodeOperatingSystem>();
+            for (int i = 0; i < 50; i++)
+            {
+                SysInfoList.Add(new NodeOperatingSystem());
+            }
+            //(MemoryLineChart.Series[0] as LineSeries).DependentRangeAxis = new LineSeriesAxis();
+            QueryTimer = new Timer(async (e) =>
+            {
+                //Debug.WriteLine("called");
+                _queryLock.WaitOne();
+                _queryLock.Reset();
+
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    var value = await CurrentNode.OperatingSystemInfo();
+                    SysInfoList.RemoveAt(0);
+                    SysInfoList.Add(value);
+                });
+                _queryLock.Set();
+            },
+            _queryLock,
+            startTimeSpan,
+            periodicTimeSpan);
+            MemoryChartAreaSeries.ValueBinding = new GenericDataPointBinding<NodeOperatingSystem, float>()
+            {
+                ValueSelector = item => ((float)item.TotalPhysicalMemorySize - (float)item.FreePhysicalMemorySize)
+                                        / (float)item.TotalPhysicalMemorySize * 100f,
+            };
+
         }
 
-        private void NodeConfigurationButton_Click(object sender, RoutedEventArgs e)
+        private void OnpropertyChanged(string name)
         {
-            Frame.Navigate(typeof(NodeConfigurationPage), CurrentNode);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        private async void NodeConfigurationButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Frame.Navigate(typeof(NodeConfigurationPage), CurrentNode);
+
+            var template = await CurrentNode.OperatingSystemInfo();
+            SysInfoList.Add(template);
+            //testCollection.Add(new TestClass() { value = 15 });
+            //((LineSeries)this.MemoryLineChart.Series[0]).ItemsSource = SysInfoList;
+            //for (int i = 0; i < SysInfoList.Count; i++)
+            //{
+            //    SysInfoList[i] = template;
+            //    SysInfoList[i].FreePhysicalMemorySize = i;
+            //}
+
+            //((LineSeries)this.MemoryLineChart.Series[0]).ItemsSource = SysInfoList;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -39,6 +103,41 @@ namespace CloudFSVisualizer
             base.OnNavigatedTo(e);
             var item = e.Parameter as HDFSNode;
             CurrentNode = item;
+            _queryLock.Set();
+
+            //SysInfoList = new ObservableCollection<NodeOperatingSystem>(new NodeOperatingSystem[20]);
+
+        }
+
+    }
+
+
+
+    public class UsedMemoryPercentageConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return 0;
+            }
+            var systemInfo = value as NodeOperatingSystem;
+            var total = systemInfo.TotalPhysicalMemorySize;
+            var free = systemInfo.FreePhysicalMemorySize;
+            //return systemInfo.FreePhysicalMemorySize / 100000;
+            if (total == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return (((float)total - (float)free) / (float)total * 100f);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
