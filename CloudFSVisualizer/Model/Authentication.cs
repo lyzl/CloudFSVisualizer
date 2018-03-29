15b586c0f30212c02b2949000c4a528111f3f233
lyzl <lyzl5764@hotmail.com>
2018-03-29 15:36:35 +0100

    Added authentication modoule for hdfs

---
+++ CloudFSVisualizer/Model/Authentication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudFSVisualizer.Model
+{
+    public class Authentication
+    {
+        public string User { get; set; }
+        public AuthenticationType type { get; set; }
+    }
+
+    public enum AuthenticationType
+    {
+        Off, Kerberos, DelegationToken
+    }
+
+}
