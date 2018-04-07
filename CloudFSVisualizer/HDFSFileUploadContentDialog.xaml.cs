7f7dd2f353a8e4940b5998cacf836486726e1d4d
lyzl <lyzl5764@hotmail.com>
2018-04-07 21:20:47 +0100

    Added upload file picker

---
+++ CloudFSVisualizer/HDFSFileUploadContentDialog.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace CloudFSVisualizer
+{
+    public sealed partial class HDFSFileUploadContentDialog : ContentDialog
+    {
+        public string RemotePath { get; set; }
+        public StorageFile PickedFile { get; set; }
+        public HDFSFileUploadContentDialog()
+        {
+            this.InitializeComponent();
+        }
+
+        public HDFSFileUploadContentDialog(string remotePath): base()
+        {
+            RemotePath = remotePath;
+            this.InitializeComponent();
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+
+        }
+
+        private async void PickFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.List,
+                SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop
+            };
+            picker.FileTypeFilter.Add("*");
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                PickedFile = file;
+                LocalFileTextBox.Text = file.Path;
+            }
+        }
+    }
+}
