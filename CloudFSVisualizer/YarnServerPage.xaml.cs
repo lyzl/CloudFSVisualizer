e7e5704abbc4e8b21de7c53cb8ffcc38e0d2c2de
lyzl <lyzl5764@hotmail.com>
2018-04-07 21:19:33 +0100

    implement yarn pages

--- CloudFSVisualizer/YarnServerPage.xaml.cs
+++ CloudFSVisualizer/YarnServerPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using CloudFSVisualizer.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,9 +24,65 @@ namespace CloudFSVisualizer
     /// </summary>
     public sealed partial class YarnServerPage : Page
     {
+        public List<YarnServer> ServerList { get; set; }
         public YarnServerPage()
         {
+            
+            ServerList = new List<YarnServer>();
+            var Server = new YarnServer()
+            {
+                SType = ServerType.HDFSServer,
+                Authentication = new HadoopAuthentication("root"),
+                ResourceManager = new YarnResourceManager
+                {
+                    Host = "h1.lingdra.com",
+                    User = "root",
+                    Pswd = "123",
+                    Description = "master",
+                    HadoopHomeDirectory = "/usr/local/hadoop/",
+                    PrivateKey = ApplicationData.Current.LocalFolder.Path + "/id_rsa_2048",
+
+
+                },
+                NodeManager = new List<YarnNodeManager>()
+                {
+                    new YarnNodeManager
+                    {
+                        Host = "h2.lingdra.com",
+                        Description = "slave",
+                        PrivateKey= ApplicationData.Current.LocalFolder.Path + "/id_rsa_2048"
+                    },
+                     new YarnNodeManager
+                    {
+                        Host = "h3.lingdra.com",
+                        Description = "slave",
+                        PrivateKey= ApplicationData.Current.LocalFolder.Path + "/id_rsa_2048"
+                    },
+                     new YarnNodeManager
+                    {
+                        Host = "h4.lingdra.com",
+                        Description = "slave",
+                        PrivateKey= ApplicationData.Current.LocalFolder.Path + "/id_rsa_2048"
+                    }
+                }
+            };
+            //GetServer();
+            ServerList.Add(Server);
+            ServerList.Add(Server);
+            //ServerManager.StoreHDFSServerListToFileAsync(ServerList);
             this.InitializeComponent();
+        }
+
+        private void ServerListGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var item = e.ClickedItem as HDFSServer;
+            Frame.Navigate(typeof(YarnServerDetailPage), item);
+        }
+
+        private void ServerListGridView_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var columns = Math.Ceiling(ActualWidth / 400);
+            //(ServerListGridView.ItemsPanelRoot as ItemsWrapGrid).ItemWidth = e.NewSize.Width / columns;
         }
     }
 }
